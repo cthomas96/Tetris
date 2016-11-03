46b2929773537745ea0fb4b10bcb8510e127dd31
APlagman <alexplagman@gmail.com>
2016-11-03 14:09:10 -0700

    libgdx Setup

---
+++ core/src/com/tetris/game/Block.java
@@ -0,0 +1,8 @@
+package com.tetris.game;
+
+/**
+ * Created on 10/31/2016.
+ * A single tile that may form part of a game piece.
+ */
+public class Block {
+}
---
+++ core/src/com/tetris/game/Piece.java
@@ -0,0 +1,8 @@
+package com.tetris.game;
+
+/**
+ * Created on 10/31/2016.
+ * Represents a contiguous group of 4 blocks as a game piece.
+ */
+public class Piece {
+}
