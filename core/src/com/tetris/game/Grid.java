e865963711d1d16a6ecdb546c40992a79e843857
ct96 <colecraypo@gmail.com>
2016-11-12 15:57:40 -0800

    Grid class

---
+++ core/src/com/tetris/game/Grid.java
@@ -0,0 +1,16 @@
+package com.tetris.game;
+
+/**
+ * Created by ColeThomasPC on 11/4/16.
+ * Main game logic goes here
+ * Height and width should be correct
+ */
+public class Grid{
+
+
+public int height = 22;
+public int width = 10;
+
+    // timer goes here
+    // fall logic
+}
